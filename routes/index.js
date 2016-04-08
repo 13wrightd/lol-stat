4b53ccea59a7364de2cd77ab7ecfba909cd2a94f
Brian West <brianwilliamwest@gmail.com>
2016-04-08 19:23:44 -0400

    Started socket.io setup

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,10 @@ var riot = require('../services/riot.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // res.render('index', { title: 'Express' });
-  // var data = riot.getChampions();
-  res.send(riot.getChampions());
+  res.render('index', { title: 'Express' });
+  // var data = riot.getChampions().then(function(d) {
+  //     res.send(d);
+  // });
 });
 
 module.exports = router;
--- services/riot.js
+++ services/riot.js
@@ -1,25 +1,45 @@
 'use strict';
 
 var https = require('https');
+var Promise = require('promise');
 
 var key = '3ceacb4c-443e-4f3b-ad9f-68c883a52a7f';
 var riot = {};
 
-riot.getChampions = function() {
-    var options = {
-        host: 'na.api.pvp.net',
-        path: '/api/lol/na/v1.2/champion?api_key=' + key,
-        method: 'GET'
-    };
+// riot.getChampions = function() {
+//     var options = {
+//         host: 'na.api.pvp.net',
+//         path: '/api/lol/na/v1.2/champion?api_key=' + key,
+//         method: 'GET'
+//     };
+//
+//     var request = https.request(options, function(response) {
+//         response.on('data', function(raw_data) {
+//             var data = raw_data.toString();
+//             console.log(data);
+//             return data;
+//         });
+//     });
+//     request.end();
+// };
 
-    var request = https.request(options, function(response) {
-        response.on('data', function(raw_data) {
-            var data = raw_data.toString();
-            console.log(data);
-            return data;
+riot.getChampions = function() {
+    return new Promise(function(resolve, reject) {
+        var options = {
+            host: 'na.api.pvp.net',
+            path: '/api/lol/na/v1.2/champion?api_key=' + key,
+            method: 'GET'
+        };
+
+        var request = https.request(options, function(response) {
+            response.on('data', function(raw_data) {
+                var data = raw_data.toString();
+                console.log(data);
+                resolve(data);
+            });
         });
+        request.end();
     });
-    request.end();
 };
 
 module.exports = riot;
