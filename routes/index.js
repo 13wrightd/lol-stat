06a7928b02bc2c39d621912b173373a65db392a1
Brian West <brianwilliamwest@gmail.com>
2016-06-15 15:47:17 -0400

    Added several riot api calls

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,16 @@ var riot = require('../services/riot.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-  // var data = riot.getChampions().then(function(d) {
-  //     res.send(d);
-  // });
+    // res.render('index', { title: 'Express' });
+    // riot.getChampions(function(data) {
+    //     res.send(data);
+    // });
+    // riot.getSummonerMasteriesByName('xTerminated', function(data) {
+    //     res.send(data);
+    // });
+    riot.getRankedStatsByName('xTerminated', function(data) {
+        res.send(data);
+    });
 });
 
 module.exports = router;
--- services/riot.js
+++ services/riot.js
@@ -1,55 +1,69 @@
 'use strict';
 
 var https = require('https');
-var Promise = require('promise');
+var config = require('../config.js');
+var key = config.apiKey.brian;
 
-var key = 'd085e5de-e971-440e-acca-dde6844736e1';
 var riot = {};
 
 riot.getChampions = function(callback) {
+    var endpoint = '/api/lol/na/v1.2/champion/';
+
+    getEndpointData(endpoint, function(data) {
+        callback(data);
+    });
+}
+
+riot.getRankedStatsByName = function(summoner, callback) {
+    var endpoint = '/api/lol/na/v1.3/stats/by-summoner';
+
+    riot.getSummonerIdByName(summoner, function(id) {
+        getEndpointData(endpoint + '/' + id + '/ranked', function(data) {
+            callback(data);
+        });
+    });
+}
+
+riot.getSummonerMasteriesByName = function(summoner, callback) {
+    var endpoint = '/api/lol/na/v1.4/summoner/by-name/' + summoner;
+    var secondEndpoint = '/api/lol/na/v1.4/summoner/';
+
+    getEndpointData(endpoint, function(data) {
+        getEndpointData(secondEndpoint + data[summoner.toLowerCase()].id + '/masteries', function(result) {
+            callback(result);
+        });
+    });
+}
+
+riot.getSummonerIdByName = function(summoner, callback) {
+    var endpoint = '/api/lol/na/v1.4/summoner/by-name/' + summoner;
+
+    getEndpointData(endpoint, function(data) {
+        callback(data[summoner.toLowerCase()].id);
+    });
+}
+
+function getEndpointData(endpoint, callback) {
+    var data = '';
+
     var options = {
-        host: 'euw.api.pvp.net',
-        path: '/api/lol/euw/v1.2/champion?api_key=' + key,
+        host: 'na.api.pvp.net',
+        path:  endpoint + '?api_key=' + key,
         method: 'GET'
     };
 
     var request = https.request(options, function(response) {
         var data = '';
 
-        console.log(response.statusCode);
-
         response.on('data', function(raw_data) {
             data += raw_data;
         });
 
         response.on('end', function() {
-            console.log(data.toString());
             callback(JSON.parse(data));
         });
     });
     request.end();
 };
-
-// riot.getChampions = function() {
-//     return new Promise(function(resolve, reject) {
-//         var options = {
-//             host: 'na.api.pvp.net',
-//             path: '/api/lol/na/v1.2/champion?api_key=' + key,
-//             method: 'GET'
-//         };
-//
-//         var request = https.request(options, function(response) {
-//             var final_data = '';
-//             response.on('data', function(chunk) {
-//                 final_data += chunk;
-//             });
-//
-//             response.on('end', function() {
-//                 resolve(JSON.parse(final_data));
-//             });
-//         });
-//         request.end();
-//     });
-// };
 
 module.exports = riot;
